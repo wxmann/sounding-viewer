a1080fea29a0f5ba2d7419cf75357798599640d5
Jim Tang <jim.z.tang@gmail.com>
2019-04-17 01:20:51 -0700

    adjust density of wind barbs on skew-t

--- src/plotting/WindBarbLiner.js
+++ src/plotting/WindBarbLiner.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import {
-  skewTWindBarbs
+  skewTWindBarbs, upperPadding
 } from '../config/container';
 import toSkewTCoord from '../transform/skewT-transform';
 import WindBarb from './WindBarb';
 import windBarbConfig from '../config/windbarb-config';
+
+function densityAdjustBarbs(data) {
+  let result = [];
+
+  let pushOb2 = (ob1, ob2) => {
+    let p0 = ob1.pressure,
+        p1 = ob2.pressure;
+      
+    return Math.abs(p0 - p1) > adjustedDeltaBarb(p0);
+  }
+
+  for (let i = 0; i < data.length; i++) {
+    let ob = data[i];
+    if (i === 0 || result.length === 0) {
+      result.push(ob);
+    } else if (pushOb2(ob, result[result.length - 1])) {
+      result.push(ob);
+    }
+  }
+  return result;
+}
+
+function adjustedDeltaBarb(p) {
+  return (p / 1000) * windBarbConfig.deltaBarb;
+}
 
 export default class WindBarbLiner extends React.Component {
   constructor(props) {
@@ -29,17 +54,21 @@ export default class WindBarbLiner extends React.Component {
     return (
       <g id="windBarbLiner">
         {
-          data.data.filter((val, i, arr) => {
-            return i % windBarbConfig.deltaBarb == 0;
-          }).filter(ob => {
-            return ob.pressure !== null && ob.speed !== null && ob.direction !== null;
-          }).map(ob => {
+          densityAdjustBarbs(
+            data.data.filter(ob => {
+              return ob.pressure !== null && ob.speed !== null && ob.direction !== null;
+            })
+          ).map(ob => {
             let p = ob.pressure;
             let windspd = ob.speed;
             let winddir = ob.direction;
 
             let yCoord = toSkewTCoord(p, 0).y;
             let xCoord = skewTWindBarbs.width / 2;
+
+            if (yCoord < windBarbConfig.barbLength) {
+              return null;
+            }
 
             return (
               <WindBarb 
